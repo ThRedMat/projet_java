7f018b3f668419964ec14a3e59d8b9981544a780
mathieu.darribau@ynov.com <mathieu.darribau@ynov.com>
2020-12-01 10:08:26 +0100

    bdd

---
+++ personnage.sql
@@ -0,0 +1,11 @@
+CREATE TABLE personnage
+(
+    id INT PRIMARY KEY NOT NULL,
+    nom VARCHAR(100),
+    capacités VARCHAR(255),
+    pv INT,
+    mana INT,
+    force INT,
+    intelligence INT,
+    charisme INT
+)
